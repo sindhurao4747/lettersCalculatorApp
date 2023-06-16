e0e512fa7e58dce8f6fd4f8a079fd1fdcd14a370
sindhurao4747 <sindhurao4747@gmail.com>
2023-06-16 06:40:11 +0000

    solution

---
+++ src/components/LettersCalculator/index.js
@@ -0,0 +1,45 @@
+// Write your code here.
+import {Component} from 'react'
+import './index.css' 
+
+class LettersCalculator extends Component {
+    state = {count: ''}
+
+    onChang = e => {
+        this.setState({count: e.target.value})
+    }
+
+    render() {
+        const {count} = this.state
+        return (
+            <div className="container">
+                <div className="card">
+                    <div className="text-container">
+                        <h1>Calculate the Letters you Enter</h1>
+                        <div>
+                            <label htmlFor="Letter">Enter the phrase</label>
+                            <br />
+                            <input
+                            type="text"
+                            placeholder="Enter the phrase"
+                            id="Letter"
+                            className="typing"
+                            onChange={this.onChang}
+                            />
+                        </div>
+                        <br />
+                        <p type="button" className="button">
+                            No of Letters: {count.length}
+                        </p>
+                    </div>
+                    <img 
+                    src="https://assets.ccbp.in/frontend/react-js/stop-watch-with-calculator-img.png"
+                    alt="Letters calculator"
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LettersCalculator
